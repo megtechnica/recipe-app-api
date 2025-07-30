08970fdab79738eef32546d96be412ba69eed911
megtechnica@gmail.com <megtechnica@gmail.com>
2025-07-29 23:30:56 -0600

    added db test commands and tests for that module

--- app/tests/test_commands.py
+++ app/tests/test_commands.py
@@ -5,3 +5,8 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    def test_wait_for_db_ready(self):
+
+
