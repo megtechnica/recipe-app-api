c56bc8c61d6e06fa43ace2fd0ebc7ae60f4b06bb
megtechnica@gmail.com <megtechnica@gmail.com>
2025-07-29 23:29:25 -0600

    added db test commands and tests for that module

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,7 @@
+"""Django command to wait for database to becmome available"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
---
+++ app/tests/test_commands.py
@@ -0,0 +1,7 @@
+from unittest.mock import patch
+
+from psyocpg2 import OperationalError as Psycopg2Error
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
